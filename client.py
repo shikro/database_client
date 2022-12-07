b85e3ffe32aac80184e7a71741bd474358d9d528
Shikhanov Roman <shikhanov.roman@gmail.com>
2022-12-07 19:45:40 +0300

    refactoring and events ui

--- client.py
+++ client.py
@@ -18,80 +18,116 @@ def password_hash(password):
 
 
 class queries:
-    get_account_info = ("SELECT a.phone, a.password "
-                        "FROM accounts AS a "
-                        "WHERE a.phone = %s "
-                        "GROUP BY a.phone, a.password")
+    @staticmethod
+    def get_account_info(phone):
+        return ("SELECT a.phone, a.password "
+                "FROM accounts AS a "
+                f"WHERE a.phone = '{phone}' "
+                "GROUP BY a.phone, a.password")
 
-    get_reader_info = ("SELECT r.reader_id, r.name, r.email, r.birth_date "
-                       "FROM readers AS r "
-                       "WHERE r.phone = %s "
-                       "GROUP BY r.reader_id, r.name, r.email, r.birth_date")
+    @staticmethod
+    def get_reader_info(phone):
+        return ("SELECT r.reader_id, r.name, r.email, r.birth_date "
+                "FROM readers AS r "
+                f"WHERE r.phone = '{phone}' "
+                "GROUP BY r.reader_id, r.name, r.email, r.birth_date")
 
-    get_speaker_info = ("SELECT s.speaker_id, s.name, s.email "
-                        "FROM speakers AS s "
-                        "WHERE s.phone = %s "
-                        "GROUP BY s.speaker_id, s.name, s.email")
+    @staticmethod
+    def get_speaker_info(phone):
+        return ("SELECT s.speaker_id, s.name, s.email "
+                "FROM speakers AS s "
+                f"WHERE s.phone = '{phone}' "
+                "GROUP BY s.speaker_id, s.name, s.email")
 
-    get_employee_info = ("SELECT e.employee_id, s.spec_id, s.name "
-                         "FROM specializations AS s "
-                         "INNER JOIN employees AS e ON s.spec_id = e.spec_id "
-                         "WHERE e.phone = %s "
-                         "GROUP BY e.employee_id, s.spec_id, s.name")
+    @staticmethod
+    def get_employee_info(phone):
+        return ("SELECT e.employee_id, s.spec_id, s.name "
+                "FROM specializations AS s "
+                "INNER JOIN employees AS e ON s.spec_id = e.spec_id "
+                f"WHERE e.phone = '{phone}' "
+                "GROUP BY e.employee_id, s.spec_id, s.name")
 
-    get_last_reader_id = "SELECT MAX(reader_id) FROM readers"
+    @staticmethod
+    def get_last_reader_id():
+        return "SELECT MAX(reader_id) FROM readers"
 
-    insert_account_step1 = ("INSERT INTO accounts "
-                            "(phone, password) "
-                            "VALUES (%s, %s)")
+    @staticmethod
+    def insert_account_step1(phone, password):
+        return ("INSERT INTO accounts "
+                "(phone, password) "
+                f"VALUES ('{phone}', '{password}')")
 
-    insert_account_step2 = ("INSERT INTO readers "
-                            "(reader_id, phone, name) "
-                            "VALUES (%s, %s, %s)")
+    @staticmethod
+    def insert_account_step2(reader_id, phone, name):
+        return ("INSERT INTO readers "
+                "(reader_id, phone, name) "
+                f"VALUES ({reader_id}, '{phone}', '{name}')")
 
-    update_account_info = ("UPDATE readers "
-                           "SET name = %s, email = %s "
-                           "WHERE phone = %s")
+    @staticmethod
+    def update_account_info(name, email, phone):
+        return ("UPDATE readers "
+                f"SET name = '{name}', email = '{email}' "
+                f"WHERE phone = '{phone}'")
 
-    update_password = ("UPDATE accounts "
-                       "SET password = %s "
-                       "WHERE phone = %s")
+    @staticmethod
+    def update_password(password, phone):
+        return ("UPDATE accounts "
+                f"SET password = '{password}' "
+                f"WHERE phone = '{phone}'")
 
-    get_all_books_id_name = ("SELECT book_id, name "
-                             "FROM books")
+    @staticmethod
+    def get_all_books_id_name():
+        return ("SELECT book_id, name "
+                "FROM books")
 
-    get_book_authors = ("SELECT a.name "
-                        "FROM authors AS a "
-                        "INNER JOIN `book authors` AS ba ON a.author_id = ba.author_id "
-                        "WHERE ba.book_id = %s")
+    @staticmethod
+    def get_book_authors(book_id):
+        return ("SELECT a.name "
+                "FROM authors AS a "
+                "INNER JOIN `book authors` AS ba ON a.author_id = ba.author_id "
+                f"WHERE ba.book_id = {book_id}")
 
-    get_book_genres = ("SELECT g.name "
-                       "FROM genres AS g "
-                       "INNER JOIN `book genres` AS bg ON g.genre_id = bg.genre_id "
-                       "WHERE bg.book_id = %s")
+    @staticmethod
+    def get_book_genres(book_id):
+        return ("SELECT g.name "
+                "FROM genres AS g "
+                "INNER JOIN `book genres` AS bg ON g.genre_id = bg.genre_id "
+                f"WHERE bg.book_id = {book_id}")
 
-    get_book_instance_by_book_id = ("SELECT bi.book_instance_id "
-                                    "FROM `book instance` AS bi "
-                                    "WHERE bi.is_available = TRUE and bi.book_id = %s "
-                                    "LIMIT 1")
+    @staticmethod
+    def get_book_instance_by_book_id(book_id):
+        return ("SELECT bi.book_instance_id "
+                "FROM `book instance` AS bi "
+                f"WHERE bi.is_available = TRUE and bi.book_id = {book_id} "
+                "LIMIT 1")
 
-    reserve_book_instance = ("UPDATE `book instance` "
-                             "SET is_available = FALSE "
-                             "WHERE book_instance_id = %s")
+    @staticmethod
+    def reserve_book_instance(book_instance_id):
+        return ("UPDATE `book instance` "
+                "SET is_available = FALSE "
+                f"WHERE book_instance_id = {book_instance_id}")
 
-    restore_book_instance = ("UPDATE `book instance` "
-                             "SET is_available = TRUE "
-                             "WHERE book_instance_id = %s")
+    @staticmethod
+    def restore_book_instance(book_instance_id):
+        return ("UPDATE `book instance` "
+                "SET is_available = TRUE "
+                f"WHERE book_instance_id = {book_instance_id}")
 
-    get_last_order_id = "SELECT MAX(order_id) FROM orders"
+    @staticmethod
+    def get_last_order_id():
+        return "SELECT MAX(order_id) FROM orders"
 
-    create_order_step1 = ("INSERT INTO orders "
-                          "(order_id, reader_id, return_date, status_id) "
-                          "VALUES (%s, %s, %s, 1)")
+    @staticmethod
+    def create_order_step1(order_id, reader_id, return_date):
+        return ("INSERT INTO orders "
+                "(order_id, reader_id, return_date, status_id) "
+                f"VALUES ({order_id}, {reader_id}, '{return_date.strftime('%Y-%m-%d %H:%M:%S')}', 1)")
 
-    create_order_step2 = ("INSERT INTO `order details` "
-                          "(order_id, book_instance_id) "
-                          "VALUES (%s, %s)")
+    @staticmethod
+    def create_order_step2(order_id, book_instance_id):
+        return ("INSERT INTO `order details` "
+                "(order_id, book_instance_id) "
+                f"VALUES ({order_id}, {book_instance_id})")
 
     @staticmethod
     def get_order_id_name(reader_id):
@@ -142,16 +178,16 @@ class db_client:
         return cursor
 
     def _update_books(self):
-        books = self._execute_query(queries.get_all_books_id_name)
+        books = self._execute_query(queries.get_all_books_id_name())
         for (book_id, name) in books:
             self.books.append(book(book_id, name))
 
         for b in self.books:
-            authors = self._execute_query(queries.get_book_authors, b.id)
+            authors = self._execute_query(queries.get_book_authors(b.id))
             for author, in authors:
                 b.authors.append(author)
 
-            genres = self._execute_query(queries.get_book_genres, b.id)
+            genres = self._execute_query(queries.get_book_genres(b.id))
             for genre, in genres:
                 b.genres.append(genre)
 
@@ -183,7 +219,7 @@ class db_client:
     def login(self, login, password):
         error_message = None
 
-        account = self._execute_query(queries.get_account_info, login)
+        account = self._execute_query(queries.get_account_info(login))
         if account.rowcount == 0:
             error_message = "No such user, please create account"
             return error_message
@@ -199,7 +235,7 @@ class db_client:
 
     def _get_account_info(self, phone):
         self.phone = phone
-        reader = self._execute_query(queries.get_reader_info, phone)
+        reader = self._execute_query(queries.get_reader_info(phone))
         if reader.rowcount:
             (reader_id, name, email, birth_date) = reader.fetchone()
             self.role = client_role.reader
@@ -209,7 +245,7 @@ class db_client:
             self.birth_date = birth_date
             return
 
-        speaker = self._execute_query(queries.get_speaker_info, phone)
+        speaker = self._execute_query(queries.get_speaker_info(phone))
         if speaker.rowcount:
             (speaker_id, name, email) = speaker.fetchone()
             self.role = client_role.speaker
@@ -218,7 +254,7 @@ class db_client:
             self.email = email
             return
 
-        employee = self._execute_query(queries.get_employee_info, phone)
+        employee = self._execute_query(queries.get_employee_info(phone))
         if employee.rowcount:
             (employee_id, spec_id, name) = employee.fetchone()
             if int(spec_id) == 1:
@@ -230,21 +266,19 @@ class db_client:
             return
 
     def create_account(self, name, phone, password):
-        account_exists = self._execute_query(queries.get_account_info, phone).rowcount == 1
+        account_exists = self._execute_query(queries.get_account_info(phone)).rowcount == 1
         if account_exists:
             return "Account already exists"
 
         # insert into accounts
-        data_to_insert = (phone, password_hash(password))
         cursor = self._connection.cursor(buffered=True)
-        cursor.execute(queries.insert_account_step1, data_to_insert)
+        cursor.execute(queries.insert_account_step1(phone, password_hash(password)))
 
         # insert into readers
-        reader_id, = self._execute_query(queries.get_last_reader_id).fetchone()
+        reader_id, = self._execute_query(queries.get_last_reader_id()).fetchone()
         reader_id += 1
-        data_to_insert = (reader_id, phone, name)
         cursor = self._connection.cursor(buffered=True)
-        cursor.execute(queries.insert_account_step2, data_to_insert)
+        cursor.execute(queries.insert_account_step2(reader_id, phone, name))
 
         # commit data
         self._connection.commit()
@@ -259,20 +293,18 @@ class db_client:
         if new_password:
             self.password = new_password
 
-        info = (self.name, self.email, self.phone)
         cursor = self._connection.cursor(buffered=True)
-        cursor.execute(queries.update_account_info, info)
+        cursor.execute(queries.update_account_info(self.name, self.email, self.phone))
 
-        info = (password_hash(self.password), self.phone)
         cursor = self._connection.cursor(buffered=True)
-        cursor.execute(queries.update_password, info)
+        cursor.execute(queries.update_password(password_hash(self.password), self.phone))
 
         self._connection.commit()
 
     def create_order(self, new_order, return_date):
         order = []
         for book_id in new_order:
-            b = self._execute_query(queries.get_book_instance_by_book_id, book_id)
+            b = self._execute_query(queries.get_book_instance_by_book_id(book_id))
             if not b.rowcount:
                 for bk in self.books:
                     if bk.id == book_id:
@@ -281,17 +313,15 @@ class db_client:
             book_instance_id, = b.fetchone()
             order.append(book_instance_id)
 
-        new_order_id, = self._execute_query(queries.get_last_order_id).fetchone()
+        new_order_id, = self._execute_query(queries.get_last_order_id()).fetchone()
         new_order_id += 1
 
         cursor = self._connection.cursor(buffered=True)
-        data_to_insert = (new_order_id, self.id, return_date)
-        cursor.execute(queries.create_order_step1, data_to_insert)
+        cursor.execute(queries.create_order_step1(new_order_id, self.id, return_date))
 
         for book_instance_id in order:
-            data_to_insert = (new_order_id, book_instance_id)
-            cursor.execute(queries.create_order_step2, data_to_insert)
-            cursor.execute(queries.reserve_book_instance, (book_instance_id,))
+            cursor.execute(queries.create_order_step2(new_order_id, book_instance_id))
+            cursor.execute(queries.reserve_book_instance(book_instance_id))
 
         self._connection.commit()
         self._update_orders()
